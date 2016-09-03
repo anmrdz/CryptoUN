6ad6c15529366ff60b44e50f48d49c123fc53d57
anmrdz <anmrdz@gmail.com>
2016-09-02 23:39:11 -0500

    added drawing of masks

--- js/turning.js
+++ js/turning.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+	
   $("#encrypt_btn").click(function() {
     $.ajax({
       type: 'GET',
@@ -10,7 +10,10 @@ $(document).ready(function() {
       success: function (data) {
         $("#ciphertext_card").removeClass("hide");
         $("#ciphertext").text(data.cipherText);
-        console.log(data.key.length);
+		for (var mask = 0; mask < 4; mask++) {
+			drawBoard(mask, data.keyMap[mask]);
+			console.log(data.keyMap[mask]);
+		}
       },
       error: function (error) {
         alert("Request failed with status " + error.status);
@@ -34,4 +37,36 @@ $(document).ready(function() {
       }
     });
   });
+
+    function drawBoard(canvas, mask){
+		$("#canvas_mask"+canvas).removeClass("hide");
+		var context = $("#canvas_mask"+canvas)[0].getContext("2d");
+		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+		var lm = mask.length
+		context.font = "35pt Roboto";
+		var board_w = 40 * lm;
+		var board_h = board_w;
+		var p = 10;
+		
+		context.beginPath();
+
+		for (var x = 0; x <= board_w; x += 40) {
+			context.moveTo(0.5 + x + p, p);
+			context.lineTo(0.5 + x + p, board_h + p);
+		}
+		
+		for (var idx = 0; idx < lm; idx++){
+			context.fillText(""+idx, (mask[idx]%lm) * 40 + 20, Math.floor(mask[idx]/lm) * 40 + 40);
+		}
+
+		for (var x = 0; x <= board_h; x += 40) {
+			context.moveTo(p, 0.5 + x + p);
+			context.lineTo(board_w + p, 0.5 + x + p);
+		}
+
+		context.strokeStyle = "black";
+		context.stroke();
+		context.closePath();
+    } 
+  
 });
