4ac30249c3ce7cfd543613c2a2a13c65c004c0b1
anmrdz <anmrdz@gmail.com>
2016-09-26 22:22:05 -0500

    Added page for DES

---
+++ js/des.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+	
+	var last_key = "", last_iv = "";
+	
+  $("#encrypt_btn").click(function() {
+	var plaintxt = $("#plaintext_input").val()
+    $.ajax({
+      type: 'GET',
+      url: 'https://cryptoun2016.appspot.com/_ah/api/criptoun/1/encryptCFBDES',
+      data: {plaintext: plaintxt},
+      contentType: "application/json; charset=utf-8",
+      traditional: true,
+      success: function (data) {
+        $("#ciphertext_card").removeClass("hide");
+        $("#ciphertext").text(data.cipherText);
+		
+		$("#iv_card").removeClass("hide");
+        $("#iv").text(data.iv);
+		
+		$("#key_card").removeClass("hide");
+        $("#key").text(data.desKeyText);
+		
+		last_key = data.desKeyText;
+		last_iv = data.iv;
+      },
+      error: function (error) {
+        alert("Request failed with status " + error.status);
+      }
+    });
+  });
+  
+  $("#decrypt_btn").click(function() {
+	
+	var key = last_key;
+	var liv = last_iv;
+	if (last_key == "custom") {
+		key = $("#customkey_input").val();
+		liv = $("#customiv_input").val();
+	}
+	
+	if($("#ciphertext_input").val() == "") {
+		Materialize.toast("The ciphertext can't be empty", 4000);
+		return;
+	}
+	
+    $.ajax({
+      type: 'GET',
+      url: 'https://cryptoun2016.appspot.com/_ah/api/criptoun/1/decryptCFBDES',
+      data: {ciphertext: $("#ciphertext_input").val(), desKeyText: key, iv: liv},
+      contentType: "application/json; charset=utf-8",
+      traditional: true,
+      success: function (data) {
+        $("#plaintext_card").removeClass("hide");
+        $("#plaintext").text(data.plainText);
+      },
+      error: function (error) {
+        Materialize.toast("Sorry your ciphertext couldn't be decrypted, check your key and iv", 4000);
+      }
+    });
+  });
+  
+  $("#type_key").change(function() {
+	 if ($("#type_key").val() == "lk") {
+		 if(last_key == "") {
+			Materialize.toast("There isn't a last key", 4000);
+		 }
+		 $("#custom_key").addClass("hide");
+	 } else if ($("#type_key").val() == "ck") {
+		 $("#custom").removeClass("hide");
+	 }
+  });
+   
+});
